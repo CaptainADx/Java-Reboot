b7789d2cfb801d63a9550c8c7b10e4faa9b37edc
CaptainADx <captainabhijeet567@gmail.com>
2025-02-23 13:43:35 +0530

    NA

--- BackTracking/nQueen.java
+++ BackTracking/nQueen.java
@@ -29,9 +29,9 @@ public class nQueen {
 
     public static void printBoard(char[][] board){
         System.out.println("--------------CHESS BOARD--------------");
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                System.out.print(board[i][j] + " ");
+        for (char[] board1 : board) {
+            for (int j = 0; j<board[0].length; j++) {
+                System.out.print(board1[j] + " ");
             }
             System.out.println();
         }
@@ -65,6 +65,7 @@ public class nQueen {
             }
             
             nQueens(board, 0);
+            
         }
     }
 }
