37035ffcef273834557a8f12edbbaef5b6d41318
CaptainADx <captainabhijeet567@gmail.com>
2025-04-27 04:15:28 +0530

    pushAtBottom

---
+++ Stacks/pushAtBottom.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class pushAtBottom {
+
+    public static void pushAtBottom(Stack<Integer> stack, int data){
+        if(stack.isEmpty()){
+            stack.push(data);
+            return;
+        }
+        int top = stack.pop();
+        pushAtBottom(stack, data);
+        stack.push(top);
+    }
+    
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s, 4);
+
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}
