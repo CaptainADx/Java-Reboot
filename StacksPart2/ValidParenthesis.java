7ea6f42ccccb53b938797c72bba026bb5672156d
CaptainADx <captainabhijeet567@gmail.com>
2025-05-03 00:13:19 +0530

    Stack Level 2 Started

---
+++ StacksPart2/ValidParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '[' || ch == '{' || ch == '('){
+                stack.push(ch);
+            } else {
+                if(!stack.isEmpty()){
+                    if((stack.peek() == '(' && ch == ')') || (stack.peek() == '{' && ch == '}') ||(stack.peek() == '[' && ch == ']')){
+                        stack.pop();
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String parenthesis = "[{()}()]{}";
+        String parenthesis1= "[{()}()]{";
+        System.out.println(isValid(parenthesis));
+        System.out.println(isValid(parenthesis1));
+    }
+}
