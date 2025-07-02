3d5346851b27dd79faf45eeb1042ba1cf28be686
CaptainADx <captainabhijeet567@gmail.com>
2025-07-02 13:09:26 +0530

    Practicing Queues

--- Queues/CircularQueueUsingArray.java
+++ Queues/CircularQueueUsingArray.java
@@ -1,19 +1,19 @@
 public class CircularQueueUsingArray {
     public static class Queue{
-        public static int[] arr;
-        public static int size;
-        public static int rear;
-        public static int front;
+        static int[] arr;
+        static int front;
+        static int rear;
+        static int size;
 
         Queue(int n){
             arr = new int[n];
-            rear = -1;
             front = -1;
+            rear = -1;
             size = n;
         }
 
         public boolean isEmpty(){
-            return (rear==-1 && front==-1);
+            return (rear == -1 && front == -1);
         }
 
         public boolean isFull(){
@@ -22,28 +22,28 @@ public class CircularQueueUsingArray {
 
         public void add(int data){
             if(isFull()){
-                System.out.println("Queue is already Full");
-                return;
+               System.out.println("Queue is Full");
+               return; 
             }
-            if(front == -1){
-                front=0;
+            if(front==-1){
+                front = 0;
             }
             rear = (rear+1)%size;
             arr[rear] = data;
         }
 
-        public int remove(){
+        public int delete(){
             if(isEmpty()){
-                System.out.println("Queue is Empty");
+                System.out.println("Queue is Already Empty");
                 return -1;
             }
-            int result = arr[front];
-            if(rear == front){
-                rear = front = -1;
-            } else {
+            int deleted = arr[front];
+            if(front == rear){
+                front = rear = -1;
+            } else{
                 front = (front+1)%size;
             }
-            return result;
+            return deleted;
         }
 
         public int peek(){
@@ -53,21 +53,21 @@ public class CircularQueueUsingArray {
             }
             return arr[front];
         }
-    }
 
+    }
     public static void main(String[] args){
-        Queue q = new Queue(3);
+        Queue q = new Queue(5);
         q.add(1);
         q.add(2);
         q.add(3);
-        System.out.println(q.remove());
         q.add(4);
-        System.out.println(q.remove());
         q.add(5);
+        q.delete();
+        q.add(6);
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
-            q.remove();
+            q.delete();
         }
     }
 }
