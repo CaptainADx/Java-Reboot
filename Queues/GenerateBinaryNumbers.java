b807886c5c7fbaf976ca355c83b5ff37bee019ab
CaptainADx <captainabhijeet567@gmail.com>
2025-07-07 14:42:21 +0530

    Generate Binary Numbers from 1 to N

---
+++ Queues/GenerateBinaryNumbers.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class GenerateBinaryNumbers {
+
+    public static ArrayList<String> generate(int n){
+        ArrayList<String> arr = new ArrayList<>();
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("1");
+
+        for(int i=0; i<n; i++){
+            String current = queue.poll();
+            arr.add(current);
+            queue.offer(current + "0");
+            queue.offer(current + "1");
+        }
+
+        return arr;
+    }  
+    
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(generate(n));
+
+    }
+}
