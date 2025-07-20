efcb9812e24d1c428d64138a38ba9a17f3af871d
CaptainADx <captainabhijeet567@gmail.com>
2025-07-20 13:51:33 +0530

    Revisiting Binary Search

---
+++ BinarySearch.java/FindLowerBound.java
@@ -0,0 +1,47 @@
+class FindLowerBound{
+    // Lower Bound is defined as Smallest index for which the Arr[i] >= X.
+
+
+    //Below is the recursive approach for the Lower Bound
+    public static int findLowerBoundOfX(int[] arr, int low, int high, int x, int res){
+        if(low > high){
+            return res;
+        }
+        
+        int mid = low + (high-low)/2;
+
+        if(arr[mid] >= x){
+            res = mid;
+            return findLowerBoundOfX(arr, low, mid-1, x, res);
+        } else{
+            return findLowerBoundOfX(arr, mid+1, high, x, res);
+        }        
+    }
+
+
+    //Below is the iterative approach for Lower Bound
+    public static int findLowerBoundOfXIteractive(int[] arr, int x){
+        int low =0, high = arr.length-1;
+        int res = arr.length;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid] >= x){
+                res = mid;
+                high = mid-1;
+            } else{
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+
+        int[] arr ={1, 2, 3, 3, 5, 8, 8, 10, 10, 11};
+        int X = 9;
+        // int LB = findLowerBoundOfX(arr, 0, arr.length-1, X, arr.length); //Recursive Approach
+        // int LB = findLowerBoundOfXIteractive(arr, X); //Iterative Approach
+        System.out.println("Lower Bound of " + X + " is --> " + LB);
+    }
+}
