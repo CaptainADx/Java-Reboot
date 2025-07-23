4d1af376ea7d4c9872838c673762d6bca7d8c403
CaptainADx <captainabhijeet567@gmail.com>
2025-07-24 02:17:16 +0530

    SubSet Sum code added

---
+++ Recursion/SubseSum_I.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SubseSum_I {
+    
+    public static void findSubsetSum(int[] arr, int idx, int sum, List<Integer> res){
+        if(idx == arr.length){
+            res.add(sum);
+            return;
+        }
+
+        findSubsetSum(arr, idx+1, sum, res);
+        findSubsetSum(arr, idx+1, sum+arr[idx], res);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,2,1};
+        int idx = 0;
+        int sum = 0;
+        List<Integer> res = new ArrayList<>();
+        findSubsetSum(arr, idx, sum, res);
+        Collections.sort(res);
+        System.out.println(res);
+    }
+}
